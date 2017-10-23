17862fbf0cf0d6e1bc8072d1f5e83ae6ce07151f
DStanarevic <DejanStanarevic@gmail.com>
2017-10-23 14:38:30 -0400

    Initial commit

---
+++ Suggestion_Box/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Suggestion_Box.Startup))]
+namespace Suggestion_Box
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
